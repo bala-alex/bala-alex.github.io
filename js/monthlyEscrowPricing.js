cea6598d956eb4ec8dfe6650e89041124a567f00
bala-alex <alexandru.balasiu@gmail.com>
2023-10-11 14:28:28 +0300

    updated pricing tags for solutions pages, escrow assets pages and other updates

--- js/monthlyEscrowPricing.js
+++ js/monthlyEscrowPricing.js
@@ -1,50 +1,53 @@
+var pathname = location.pathname;
+var pricesData = localStorage.getItem('prices');
+var prices = JSON.parse(pricesData);
 
-  $( document ).ready(function() {
-    $.ajax('https://ipapi.co/json')
-    .then(
-      function success(response) {
-        if (response.timezone && (response.timezone).includes('Europe') && (response.currency !== 'GBP') && (response.currency !== 'CHF')) {
-          $('.software-escrow-monthly').text('€89');
-          $('.saas-escrow-monthly').text('€199');
-        }
-        else if (response.currency === 'GBP') {
-          
-          $('.software-escrow-monthly').text('£79');
-          $('.saas-escrow-monthly').text('£149');
-        }
-        else if (response.currency === 'INR') {
-          $('.software-escrow-monthly').text('₹6,995');
-          $('.saas-escrow-monthly').text('₹12,500');
-        }
-        else if (response.currency === 'AUD') {
-          $('.software-escrow-monthly').text('A$109');
-          $('.saas-escrow-monthly').text('A$250');
-        }
-
-        else if (response.currency === 'SGD') {
-          $('.software-escrow-monthly').text('$109');
-          $('.saas-escrow-monthly').text('$249');
-        }
-
-        else if (response.currency === 'ZAR') {
-          $('.software-escrow-monthly').text('R1,395');
-          $('.saas-escrow-monthly').text('R2,799');
-        }
-
-        else if (response.currency === 'CHF') {
-          $('.software-escrow-monthly').text('CHF89');
-          $('.saas-escrow-monthly').text('CHF219');
-        }
-
-        else if (response.currency === 'CAD') {
-          $('.software-escrow-monthly').text('CAD119');
-          $('.saas-escrow-monthly').text('CAD299');
-        }
-      },
-      function fail(data, status) {
-          console.log('Request failed.  Returned status of',
-                      status);
+$( document ).ready(function() {
+  $.ajax('https://ipapi.co/json')
+  .then(
+    function success(response) {
+      if (response.timezone && (response.timezone).includes('Europe') && (response.currency !== 'GBP') && (response.currency !== 'CHF')) {
+        $('.software-escrow-monthly').text(prices.software_escrow_monthly.EUR);
+        $('.saas-escrow-monthly').text(prices.saas_escrow_monthly.EUR);
       }
-    )
-    .catch((e) => console.log(e))
-  });
+      else if (response.currency === 'GBP') {
+        
+        $('.software-escrow-monthly').text(prices.software_escrow_monthly.GBP);
+        $('.saas-escrow-monthly').text(prices.saas_escrow_monthly.GBP);
+      }
+      else if (response.currency === 'INR') {
+        $('.software-escrow-monthly').text(prices.software_escrow_monthly.INR);
+        $('.saas-escrow-monthly').text(prices.saas_escrow_monthly.INR);
+      }
+      else if (response.currency === 'AUD') {
+        $('.software-escrow-monthly').text(prices.software_escrow_monthly.AUD);
+        $('.saas-escrow-monthly').text(prices.saas_escrow_monthly.AUD);
+      }
+
+      else if (response.currency === 'SGD') {
+        $('.software-escrow-monthly').text(prices.software_escrow_monthly.SGD);
+        $('.saas-escrow-monthly').text(prices.saas_escrow_monthly.SGD);
+      }
+
+      else if (response.currency === 'ZAR') {
+        $('.software-escrow-monthly').text(prices.software_escrow_monthly.ZAR);
+        $('.saas-escrow-monthly').text(prices.saas_escrow_monthly.ZAR);
+      }
+
+      else if (response.currency === 'CHF') {
+        $('.software-escrow-monthly').text(prices.software_escrow_monthly.CHF);
+        $('.saas-escrow-monthly').text(prices.saas_escrow_monthly.CHF);
+      }
+
+      else if (response.currency === 'CAD') {
+        $('.software-escrow-monthly').text(prices.software_escrow_monthly.CAD);
+        $('.saas-escrow-monthly').text(prices.saas_escrow_monthly.CAD);
+      }
+    },
+    function fail(data, status) {
+        console.log('Request failed.  Returned status of',
+                    status);
+    }
+  )
+  .catch((e) => console.log(e))
+});
